0197d1d4e1c17d19ff224c0d71c2f3bf42b4195b
Joshua Bidwell <jjbidwell262@gmail.com>
2020-10-14 09:00:10 -0700

    Added app.listen and basic get route

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+app.get('*', (req, res) => {
+    res.send('<h1>cheemsburbger</h1>');
+});
+
+app.listen(PORT, () => {
+    // Log (server-side) when our server has started
+    console.log(`Server listening on: http://localhost:${ PORT}`);
+  });
