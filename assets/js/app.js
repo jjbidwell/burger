7c2c7f524a9e5756b4120a8f31d6a9f6061bd5f8
Joshua Bidwell <jjbidwell262@gmail.com>
2020-10-15 20:23:45 -0700

    Trying to get the jquery click to work.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,17 @@
-console.log($('#eaten-list').children().length);
+$(document).ready(function(){
 
-if($('#eaten-list').children().length < 1){
-    $('#eaten').hide()
-}
+    if($('#eaten-list').children().length < 1){
+        $('#eaten').hide()
+    }
 
-if($('#to-eat-list').children().length < 1){
-    $('#to-eat').hide()
-}
+    if($('#to-eat-list').children().length < 1){
+        $('#to-eat').hide()
+    }
 
-$('h1').on('click', ()=> {
-    console.log('jquery connected');
+    $('.delete-btn').on('click', () => {
+        const id = $('#btn1').attr("class")
+        console.log('delete')
+        //console.log($(this).data("index"));
+        console.log(id);
+    })
 })
